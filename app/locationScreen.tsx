0bb0581316afb748290e9604bb794294d1946291
Katarzyna Godziek <brushandspatula@gmail.com>
2025-12-25 22:01:32 +0100

    added fetching current location

--- app/locationScreen.tsx
+++ app/locationScreen.tsx
@@ -1,17 +1,44 @@
-import { Button, Text, View } from 'react-native';
+import * as Location from "expo-location";
+import React from "react";
+import { Button, Text, View } from "react-native";
 
 export default function LocationScreenPage() {
+  const [location, setLocation] = React.useState<string | null>(null);
+  const [errorMsg, setErrorMsg] = React.useState<string | null>(null);
+
+  async function fetchLocation() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(
+      `Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}`
+    );
+  }
+
+  let displayMessage = "Loading location...";
+  if (errorMsg) {
+    displayMessage = errorMsg;
+  } else if (location) {
+    displayMessage = JSON.stringify(location);
+  }
 
   return (
-    <View style={{ flex: 1, padding: 24, marginTop:20, gap:15 }}>
+    <View style={{ flex: 1, padding: 24, marginTop: 20, gap: 15 }}>
       <Text style={{ fontSize: 44, marginBottom: 20 }}>
         Your current GPS Location
       </Text>
-
       <Button
         title="Get your Location!"
-        //todo tu pobieranie lokalizacji GPS
+        onPress={() => {
+          fetchLocation();
+        }}
       />
+
+      <Text style={{ fontSize: 18 }}>{displayMessage}</Text>
 
       <Button
         title="Go to Notes"
