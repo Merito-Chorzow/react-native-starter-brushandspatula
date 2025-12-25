a205517803fabe4a031b8d5fc8e180a156cd6edd
Katarzyna Godziek <brushandspatula@gmail.com>
2025-12-25 17:53:43 +0100

    added onboarding screen

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { View, Text, Button } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function OnboardingScreenPage() {
+  const router = useRouter();
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', padding: 24 }}>
+      <Text style={{ fontSize: 48, fontWeight: 'bold', marginBottom: 16 }}>
+        GPS notes for Adventurers
+      </Text>
+
+      <Text style={{ fontSize: 44, marginBottom: 24 }}>
+        React Native App
+      </Text>
+
+      <Button
+        title="Get Started!"
+        //tu bedzie routing do location page
+      />
+    </View>
+  );
+}
