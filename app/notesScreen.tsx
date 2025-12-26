9a3a218465d84573f61ef08e5b246ef3b76b4462
Katarzyna Godziek <brushandspatula@gmail.com>
2025-12-26 13:29:44 +0100

    added fetching data from JSON placeholder API

--- app/notesScreen.tsx
+++ app/notesScreen.tsx
@@ -1,9 +1,16 @@
 import NetInfo from "@react-native-community/netinfo";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+
+type Note = {
+  id: number;
+  title: string;
+};
 
 export default function NotesScreenPage() {
-  const [isOnline, setOnline] = useState<boolean>(true);
+  const [isOnline, setOnline] = useState(true);
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const checkOnlineStatus = NetInfo.addEventListener((state) => {
@@ -14,6 +21,26 @@ export default function NotesScreenPage() {
     };
   }, []);
 
+  useEffect(() => {
+    if (isOnline) {
+      getNotes();
+    }
+  }, [isOnline]);
+
+  const getNotes = async () => {
+    try {
+      setError(null);
+
+      const response = await fetch(
+        "https://my-json-server.typicode.com/typicode/demo/posts"
+      );
+      const data: Note[] = await response.json();
+      setNotes(data);
+    } catch (err) {
+      setError("Failed to fetch notes.");
+    }
+  };
+
   let displayMessage = "";
 
   {
@@ -23,9 +50,22 @@ export default function NotesScreenPage() {
   }
 
   return (
-    <View style={{ flex: 1, padding: 24, marginTop: 20 }}>
-      <Text style={{ fontSize: 44, marginBottom: 20 }}>Notes Screen</Text>
-      <Text style={{ fontSize: 18 }}>{displayMessage}</Text>
+    <View style={{ flex: 1, padding: 24 }}>
+      <Text style={{ fontSize: 44, marginBottom: 20 }}>Notes</Text>
+
+      <FlatList
+        data={notes}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={{ marginBottom: 16 }}>
+            <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+              {item.title}
+            </Text>
+          </View>
+        )}
+      />
+
+      {error && <Text style={{ color: "red" }}>{error}</Text>}
     </View>
   );
 }
